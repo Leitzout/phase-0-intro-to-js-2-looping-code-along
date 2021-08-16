8a45ceecc1e34d6043415f0f06e5179946ace4d7
Kaylee Marie Leitzel <leitz@Leitzout.localdomain>
2021-08-15 20:24:07 -0600

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function countDown() {
+    for (let countDown = 10; countDown > -1; countDown--) {
+        console.log(countDown);
+    }
+}
+countDown(10);
+
+const newArr = []
+function writeCards(names, event) {
+    for(let i = 0; i < names.length; i++) {
+      newArr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArr;
+}
+writeCards;
+
+
+ 
